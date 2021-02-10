fb86a534e49e5fc7188a2805ac727ba41d1332cc
Martin <martinsung00@gmail.com>
2021-02-09 21:59:44 -0800

    Fix import casing.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,4 +1,4 @@
-import App from "./components/App";
+import App from "./components/app";
 import * as React from "react";
 import { render } from "react-dom";
 
--- test/client/components/app.unit.test.tsx
+++ test/client/components/app.unit.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import App from "../../../src/client/components/App";
+import App from "../../../src/client/components/app";
 import { shallow, configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Axios from "axios";
